859983cfd157392689ff5de45788e2f9d3afe8e3
Tamir Sida <tamir.sida@post.runi.ac.il>
2025-09-15 01:25:08 +0300

    sessions

--- components/public/homepage.tsx
+++ components/public/homepage.tsx
@@ -642,7 +642,7 @@ function AlphaBetHomepageContent() {
                 )}
                 
                 {/* Info Session Button */}
-                <div className="hidden lg:block mb-6">
+                <div className="mb-6">
                   <Link href="/info-session">
                     <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-medium cursor-pointer" style={{ fontFamily: "'Gunplay', 'Black Ops One', cursive" }}>
                       Live & Pre-Recorded Info Sessions
