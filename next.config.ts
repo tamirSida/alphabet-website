db91dbef6e1e2dd9c607182460955c2081cb96d6
Tamir Sida <tamir.sida@post.runi.ac.il>
2025-12-08 20:34:00 +0200

    45

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    loader: 'default',
+    unoptimized: false,
+  },
+};
 
 export default nextConfig;
